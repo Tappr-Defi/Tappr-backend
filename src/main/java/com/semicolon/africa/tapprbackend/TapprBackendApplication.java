ecdae9de86c8de7cfcf16e88e8d63eefc5a0a5f4
Mr-Stanley <Stanleyugoo5@gmail.com>
2025-06-18 21:35:54 +0100

    first commit

---
+++ src/main/java/com/semicolon/africa/tapprbackend/TapprBackendApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.tapprbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapprBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapprBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/africa/tapprbackend/TapprBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.tapprbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapprBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
