79b78ffe602e8747154f261e385b454d7b1b77bb
Adetayo <twinebravo@gmail.com>
2025-06-24 15:32:35 +0100

    Updated test cases for TransactionServiceImpl

--- src/main/java/com/semicolon/africa/tapprbackend/tapprException/TapprException.java
+++ src/main/java/com/semicolon/africa/tapprbackend/tapprException/TapprException.java
@@ -1,6 +1,6 @@
 package com.semicolon.africa.tapprbackend.tapprException;
 
-public class TapprException extends Throwable{
+public class TapprException extends RuntimeException{
     public TapprException(String message) {
         super(message);
     }
--- src/main/java/com/semicolon/africa/tapprbackend/transaction/exceptions/MerchantNotFoundException.java
+++ src/main/java/com/semicolon/africa/tapprbackend/transaction/exceptions/MerchantNotFoundException.java
@@ -2,7 +2,7 @@ package com.semicolon.africa.tapprbackend.transaction.exceptions;
 
 import com.semicolon.africa.tapprbackend.tapprException.TapprException;
 
-public class MerchantNotFoundException extends TapprException {
+public class MerchantNotFoundException extends RuntimeException {
     public MerchantNotFoundException(String message) {
 
         super(message);
