00aacda71022723e3b34321801e2900dae6a9983
johdanike <chinedunomeh2015@gmail.com>
2025-12-09 09:17:13 +0100

    Changedclient port in config

--- src/main/java/com/tappr/finance/tapprbackend/onboarding/dtos/responses/RegisterUserResponse.java
+++ src/main/java/com/tappr/finance/tapprbackend/onboarding/dtos/responses/RegisterUserResponse.java
@@ -1,17 +1,16 @@
 package com.tappr.finance.tapprbackend.onboarding.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Map;
 
-@Setter
-@Getter
-//@RequiredArgsConstructor
-@NoArgsConstructor
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterUserResponse {
     private boolean success;
     private String message;
@@ -19,21 +18,4 @@ public class RegisterUserResponse {
     private String username;
     private int statusCode;
     private Map<String, Object> responseBody;
-
-
-    public RegisterUserResponse(boolean b, String loginSuccessful, String suiAddress, String userName) {
-        this.success = b;
-        this.message = loginSuccessful;
-        this.suiAddress = suiAddress;
-        this.username = userName;
-    }
-
-    public RegisterUserResponse(boolean isSuccess, String registrationSuccessful, String suiAddress, String userName, int value, Map<String, Object> walletDetails) {
-        this.success = isSuccess;
-        this.message = registrationSuccessful;
-        this.suiAddress = suiAddress;
-        this.username = userName;
-        this.statusCode = value;
-        this.responseBody = walletDetails;
-    }
 }
